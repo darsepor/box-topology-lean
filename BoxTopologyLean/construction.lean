53935f45c7c89f3d8a2755935e82242bf2a8ea19
darsepor <darijus.sep@gmail.com>
2025-05-15 13:50:39 +0300

    proj cont progress

--- BoxTopologyLean/construction.lean
+++ BoxTopologyLean/construction.lean
@@ -6,22 +6,72 @@ import Mathlib
 namespace BoxTopology
 open Set Filter TopologicalSpace Topology
 
-def boxTopology {ι : Type*} {Y : ι → Type*} [t : ∀ i, TopologicalSpace (Y i)] :
+#check continuous_induced_rng
+
+
+instance boxTopology {ι : Type*} {Y : ι → Type*} [t : ∀ i, TopologicalSpace (Y i)] :
     TopologicalSpace ((i : ι) → Y i) :=
   .generateFrom <|
-  {B : Set ((i : ι) → Y i)|∀ (idx : ι), IsOpen ((fun (point : (k : ι) → Y k) => point idx) '' B)∧
-    B = Set.pi (Set.univ : Set ι) (fun idx => (fun point => point idx) '' B) }
+  /-{B : Set ((i : ι) → Y i)|∀ (idx : ι), IsOpen ((fun (point : (k : ι) → Y k) => point idx) '' B)∧
+    B = Set.pi (Set.univ : Set ι) (fun idx => (fun point => point idx) '' B) }-/
+    { B | ∃ (U : ∀ i, Set (Y i)), (∀ i, IsOpen (U i)) ∧ B = Set.pi Set.univ U }
 
-#check continuouos_induced_rng
+/--/
+instance boxTopology' {ι : Type*} {Y : ι → Type*} [t: ∀ i, TopologicalSpace (Y i)] :
+    TopologicalSpace ((i : ι) → Y i) where
+  IsOpen B := ∀ (point : (i :ι)  → Y i), point ∈ B → ∃ (U : (i:ι) → Set (Y i)),
+           (∀ i, IsOpen (U i)) ∧ (∀ i, point i ∈ U i) ∧ (Set.pi Set.univ U ⊆ B)
+  isOpen_univ := by
+    intro point inu
+
+    use fun i => univ
+    simp
+  isOpen_sUnion := by
+
+  isOpen_inter := sorry
+
+theorem boxTopology_eq_boxTopology' {ι : Type*} {Y : ι → Type*} [t: ∀ i, TopologicalSpace (Y i)] :
+    boxTopology = @boxTopology' ι Y t := by
+  rw[boxTopology]
+  rw[boxTopology']
+  refine TopologicalSpace.ext ?_
+  sorry
+-/
+lemma open_preimage_box {ι : Type*} {Y : ι → Type*} [DecidableEq ι]
+  [t : ∀ i : ι, TopologicalSpace (Y i)] {k : ι} (s : Set (Y k)) (f := fun (x : (i : ι) → Y i) => x k):
+  IsOpen s → IsOpen (f⁻¹' s) := by
+  intro op
+  rw[IsOpen]
+  rw[TopologicalSpace.IsOpen]
+  rw[boxTopology]
+  rw[generateFrom]
+  simp
+  refine GenerateOpen.basic (f ⁻¹' s) ?_
+  simp
+
+  use fun i => if h : i = k then h.symm ▸ s else Set.univ --gemini
+  simp
+  constructor
+  · intro idx
+    by_cases ieqk : idx = k
+    · rw[ieqk]
+      simp
+      exact op
+
+    · split_ifs
+      exact isOpen_univ
+
+  · rw[Set.preimage]
+
+    sorry
+
+
 lemma box_continuity_transfer  {ι : Type*} {Y : ι → Type*}
-    [t : ∀ i : ι, TopologicalSpace (Y i)] (j: ι) (box : boxTopology):
-    Continuous fun (x : Π i, Y i) ↦ x j:= by
-
-
+    [t : ∀ i : ι, TopologicalSpace (Y i)] (j: ι):
+    Continuous fun (point : (k : ι) → Y k) => point j := by
   sorry
 
-
-lemma identity_continuity
+--lemma identity_continuity
 
 lemma box_topology_is_finer {ι : Type*} {Y : ι → Type*}
           [t : ∀ i : ι, TopologicalSpace (Y i)] :
